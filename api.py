94946fe73bc21359b8d0f979ef05a79f9d061d5b
bp289 <birajpanta112@gamil.com>
2023-01-10 15:13:27 +0000

    finished endpoint

--- api.py
+++ api.py
@@ -15,7 +15,6 @@ def get_db_connection():
 
 @app.route("/", methods=['GET'])
 def index():
-
     conn = get_db_connection()
     cur = conn.cursor(cursor_factory=pse.RealDictCursor)
     cur.execute("SELECT * FROM users")
@@ -24,3 +23,34 @@ def index():
     cur.close()
 
     return users_data
+
+def query_database(query, search_param):
+    conn = get_db_connection()
+    with conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor) as cursor:
+        try:
+            cursor.execute(query, (search_param,))
+            return cursor.fetchall()
+        except(Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            if conn:
+                cursor.close()
+    
+@app.route("/getUserQR", methods=['GET'])
+def getUserQR():
+    args =  request.args
+    if "userName" in args:
+        userName = args["userName"]
+        print(userName)
+        qrCode  = query_database(
+                """WITH userQR AS ( 
+                    SELECT cards.user_id, 
+                    cards.id, 
+                    users.username 
+                    FROM cards, users WHERE users.id=cards.user_id
+                ) 
+                SELECT id from userQR WHERE username=%s""", userName)
+        print(qrCode)
+        return jsonify(qrCode)
+    else:
+        return jsonify({status : 400, reason: "invalid"})
