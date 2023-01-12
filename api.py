a24645845edaf18663bb2a76c62424e57a8809b4
Will <trainee.william.simms@sigmalabs.co.uk>
2023-01-12 14:57:54 +0000

    test

--- api.py
+++ api.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 from dotenv import load_dotenv
 import os
 import bcrypt 
-
+  
 app=Flask(__name__)
 CORS(app)
 
@@ -152,4 +152,4 @@ def login_user():
         return "username or password incorrect", 403
 
 if __name__ == '__main__':
-    app.run() 
+    app.run()
