d341219ec8d516f448fdb3dcce6dd629b7ab3dd1
Will <trainee.william.simms@sigmalabs.co.uk>
2023-01-19 14:38:28 +0000

    changed function names in tests/test_view-card

--- tests/test_view-card.py
+++ tests/test_view-card.py
@@ -15,21 +15,21 @@ def test_willsimms_view_card(): # viewing card 6 is successful
     assert response.status == '200 OK'
     assert json.loads(response.data.decode('utf-8'))[0] == card_6_response
 
-def test_no_username(): # no username supplied is successful
+def test_no_username_view_card(): # no username supplied is successful
     response = app.test_client().post('/view-card/6', json={"username": ""})
     assert response.status == '200 OK'
     assert json.loads(response.data.decode('utf-8'))[0] == card_6_response
 
-def test_non_user(): # non-existent user is successful
+def test_non_user_view_card(): # non-existent user is successful
     response = app.test_client().post('/view-card/6', json={"username": "notauser"})
     assert response.status == '200 OK'
     assert json.loads(response.data.decode('utf-8'))[0] == card_6_response
 
-def test_non_card(): # non-existent card is successful (returns [])
+def test_non_card_view_card(): # non-existent card is successful (returns [])
     response = app.test_client().post('/view-card/10000', json={"username": "willsimms"})
     assert response.status == '200 OK'
     assert not json.loads(response.data.decode('utf-8'))
 
-def test_bad_request(): # no username key sent in body fails.
+def test_bad_request_view_card(): # no username key sent in body fails.
     response = app.test_client().post('/view-card/6', json={"notusername": "willsimms"})
     assert response.status == '500 INTERNAL SERVER ERROR'
