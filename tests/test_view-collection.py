ef7db8ff42d3d15b5f3b915cb59c81ba40eae8d7
Will <trainee.william.simms@sigmalabs.co.uk>
2023-01-19 12:05:25 +0000

    view-collection tests not working currently

--- tests/test_view-collection.py
+++ tests/test_view-collection.py
@@ -2,3 +2,7 @@ import pytest
 import json
 from app import app
 
+def test_willsimms_collection(): 
+    response = app.test_client().post("/view-collection", json={"username": "willsimms"})
+    assert response.status == '200 OK'
+    # assert json.loads(response.data.decode('utf-8'))[0] == willsimms_first_scan
