7b678ee1b31272cc3b79cf4af6886394970ec697
Will <trainee.william.simms@sigmalabs.co.uk>
2023-01-19 15:28:38 +0000

    view collection test works when ran independently

--- tests/test_view-collection.py
+++ tests/test_view-collection.py
@@ -5,4 +5,4 @@ from app import app
 def test_willsimms_collection(): 
     response = app.test_client().post("/view-collection", json={"username": "willsimms"})
     assert response.status == '200 OK'
-    # assert json.loads(response.data.decode('utf-8'))[0] == willsimms_first_scan
+    assert json.loads(response.data.decode('utf-8'))[0]["id"] == 6
